8178179d9f675803e4875afcb0f99332d4b00ca4
bor <bor.brudar@gmail.com>
2024-02-18 06:58:51 +0100

    gay triangle

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate sdl2;
 extern crate gl;
 
+pub mod render_gl;
 use sdl2::pixels::Color;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
@@ -9,11 +10,17 @@ use std::time::Duration;
 pub fn main() {
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
+    
+    // set opengl version 4.5
+    let gl_attr = video_subsystem.gl_attr();
+    
+    gl_attr.set_context_profile(sdl2::video::GLProfile::Core);
+    gl_attr.set_context_version(3,3);
 
     let window = video_subsystem
-        .window("Ray marcher fr", 800, 600)
-        .opengl()
-        .resizable()
+    .window("Ray marcher fr", 800, 600)
+    .opengl()
+    .resizable()
         .position_centered()
         .build()
         .unwrap();
@@ -21,20 +28,83 @@ pub fn main() {
     let gl_context = window.gl_create_context().unwrap();
     let gl = gl::load_with(|s| video_subsystem.gl_get_proc_address(s) as *const std::os::raw::c_void);
 
+    unsafe{
+        gl::Viewport(0,0,900,700);
+        gl::ClearColor(0.3,0.3,0.5,1.0);
+    }
+
 
     let mut event_pump = sdl_context.event_pump().unwrap();
-    let mut ii = 0;
-    unsafe {
-        gl::ClearColor(0.3, 0.3, 0.5, 1.0);
-    }
     
+    use std::ffi::CString;
+    let vert_shader = render_gl::Shader::from_vert_source(
+        &CString::new(include_str!("triangle.vert")).unwrap()
+    ).unwrap();
+    
+    let frag_shader = render_gl::Shader::from_frag_source(
+        &CString::new(include_str!("triangle.frag")).unwrap()
+    ).unwrap();
+    
+    let shader_program = render_gl::Program::from_shaders(
+        &[vert_shader,frag_shader]
+    ).unwrap();
+
+
+    let vertices: Vec<f32> = vec![
+    // positions      // colors
+    0.5, -0.5, 0.0,   1.0, 0.0, 0.0,   // bottom right
+    -0.5, -0.5, 0.0,  0.0, 1.0, 0.0,   // bottom left
+    0.0,  0.5, 0.0,   0.0, 0.0, 1.0    // top
+    ];
+
+    let mut vbo: gl::types::GLuint = 0;
+    unsafe {
+        gl::GenBuffers(1, &mut vbo);
+    }
+
+    unsafe{
+        gl::BindBuffer(gl::ARRAY_BUFFER,vbo);
+        gl::BufferData(
+            gl::ARRAY_BUFFER,
+            (vertices.len() * std::mem::size_of::<f32>()) as gl::types::GLsizeiptr, // size of data in bytes
+            vertices.as_ptr() as *const gl::types::GLvoid, // pointer to data
+            gl::STATIC_DRAW, // usage
+        );
+        gl::BindBuffer(gl::ARRAY_BUFFER, 0); // unbind the buffer
+    }
+
+    let mut vao: gl::types::GLuint = 0;
+    unsafe {
+        gl::GenVertexArrays(1, &mut vao);
+        gl::BindVertexArray(vao);
+        gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+        
+        gl::EnableVertexAttribArray(0); // this is "layout (location = 0)" in vertex shader
+        gl::VertexAttribPointer(
+            0, // index of the generic vertex attribute ("layout (location = 0)")
+            3, // the number of components per generic vertex attribute
+            gl::FLOAT, // data type
+            gl::FALSE, // normalized (int-to-float conversion)
+            (6 * std::mem::size_of::<f32>()) as gl::types::GLint, // stride (byte offset between consecutive attributes)
+            std::ptr::null() // offset of the first component
+        );
+        
+        gl::EnableVertexAttribArray(1); // this is "layout (location = 0)" in vertex shader
+        gl::VertexAttribPointer(
+            1, // index of the generic vertex attribute ("layout (location = 0)")
+            3, // the number of components per generic vertex attribute
+            gl::FLOAT, // data type
+            gl::FALSE, // normalized (int-to-float conversion)
+            (6 * std::mem::size_of::<f32>()) as gl::types::GLint, // stride (byte offset between consecutive attributes)
+            (3 * std::mem::size_of::<f32>()) as *const gl::types::GLvoid // offset of the first component
+        );
+
+        
+        gl::BindBuffer(gl::ARRAY_BUFFER, 0);
+        gl::BindVertexArray(0);
+    }
+
     'main: loop {
-        ii = (ii + 1) % 255;
-        let i : f32 = ii as f32;
-        unsafe {
-            gl::ClearColor(i/255.0, 64.0/255.0, (255.0-i)/255.0, 1.0);
-        }
-        //canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
         for event in event_pump.poll_iter() {
             match event {
                 Event::Quit {..} |
@@ -44,12 +114,20 @@ pub fn main() {
                 _ => {}
             }
         }
-        
+
+        shader_program.set_used();
         unsafe{
             gl::Clear(gl::COLOR_BUFFER_BIT);
+            gl::BindVertexArray(vao);
+            gl::DrawArrays(
+                gl::TRIANGLES, // mode
+                0, //starting index in the enabled arrays
+                3 // number of indices to be rendered
+            );
+
         }
         window.gl_swap_window();
-
+        
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
     }
 }
