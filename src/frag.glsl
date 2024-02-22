fc635cdfe5a01fdc7d5552da050d7a183a69239e
bor <bor.brudar@gmail.com>
2024-02-22 01:29:54 +0100

    wrong normals

--- src/frag.glsl
+++ src/frag.glsl
@@ -1,4 +1,5 @@
 #version 330 core
+//#include hg_sdf.glsl
 
 uniform vec2 u_resolution;
 in vec2 fragCoord;
@@ -9,14 +10,30 @@ const float FOV = 1.0;
 const int MAX_STEPS = 256;
 const float MAX_DIST = 500;
 
+
+float fPlane(vec3 p, vec3 n, float distanceFromOrigin) {
+	return dot(p, n) + distanceFromOrigin;
+}
+
+vec2 fOpUnionID(vec2 res1,vec2 res2){
+    return (res1.x < res2.x) ? res1 : res2;
+}
+
 vec2 map(vec3 p){
-    p = mod(p,4.0) - 4.0 * 0.5;
+    //plane
+    float planeDist= fPlane(p,vec3(0.0,1.0,0.0),1.0);
+    //float planeDist= 2;
+    float planeID = 2.0;
+    vec2 plane = vec2(planeDist,planeID);
     //sphere
+    //p = mod(p,4.0) - 4.0 * 0.5; // infinite repetition
     float sphereDist = length(p) - 1.0;
     float sphereID = 1.0;
     vec2 sphere = vec2(sphereDist,sphereID);
+
     //result
-    vec2 res = sphere;
+    vec2 res = fOpUnionID(sphere,plane);
+    //vec2 res = sphere;
     return res;
 }
 
@@ -32,14 +49,31 @@ vec2 rayMarch(vec3 ro, vec3 rd){
     return object;
 }
 
+vec3 getNormal(vec3 p){
+    vec2 e = vec2(EPSILON,0.0);
+    vec3 n = vec3(map(p).x) - vec3(map(p-e.xyy).x, 
+    map(p-e.yxy).x, map(p-e.yyx).x);
+    return normalize(n);
+}
+
+vec3 getLight(vec3 p, vec3 rd,vec3 color){
+    vec3 lightPos = vec3(20.0,40.0,-30.0);
+    vec3 L = normalize(lightPos - p);
+    vec3 N = getNormal(p);
+
+    vec3 diffuse = color * clamp(dot(L,N),0.0,1.0);
+    return N;
+}
 
 void render(inout vec3 col, in vec2 uv){
-    vec3 ray_origin = vec3(0.0,0.0,-3.0);
-    vec3 ray_dir = normalize(vec3(uv,FOV));
+    vec3 ro = vec3(0.0,0.0,-3.0);
+    vec3 rd = normalize(vec3(uv,FOV));
 
-    vec2 object = rayMarch(ray_origin,ray_dir);
+    vec2 object = rayMarch(ro,rd);
     if(object.x < MAX_DIST){
-        col += 3.0/ object.x;
+        col += 3.0 / object.x;
+        vec3 p = ro + object.x * rd;
+        col += getLight(p,rd,vec3(1));
     }
 }
 
